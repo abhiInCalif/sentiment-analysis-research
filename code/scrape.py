4afe3b80a2ef861b60ab71ee72b8987e494b997f
Abhinav Khanna <akhanna@princeton.edu>
2014-11-02 20:37:28 -0500

    updated scraper to have all the scraping done in-line

--- code/scrape.py
+++ code/scrape.py
@@ -19,7 +19,7 @@ def scrape():
 
     print 'Grabbing all hte movies'
     for query_term in query_terms:
-        for i in range(1,64):
+        for i in range(1,20):
             # get the first 15 pages of rotten tomatoes data set
             url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json'
             url = url + '?apikey=qpbuunhuhmmp9xsapp3gjcp2&q=' + str(query_term) + '&page=' + str(i)
@@ -30,70 +30,59 @@ def scrape():
                 continue
 
             for movie in movies:
-                reviews_array.append(movie['links']['reviews'])
+                # second option, lets do it all streamlined
+                link = movie['links']['reviews']
+                if link is None:
+                    continue
 
+                response = requests.get(str(link) + '?apikey=' + apikey)
+                json_response = json.loads(response.text)
 
-    # now iterate through the review links and scrape the data on the review
-    print 'Grabbed ' + str(len(reviews_array)) + ' movies'
-    print 'Grabbing the reviews'
-    for link in reviews_array:
-        if link is None:
-            continue
+                reviews = json_response.get('reviews')
+                if reviews is None:
+                    # this is way to high, needs to be looked into and fixed
+                    import pdb; pdb.set_trace()
+                    print json_response
+                    empty_count = empty_count + 1
+                    continue
+                
+                for review in reviews:
+                    review_publication = review['publication']
 
-        response = requests.get(str(link) + '?apikey=' + apikey)
-        json_response = json.loads(response.text)
+                    if review_publication not in accepted_publications:
+                        forgoed_count = forgoed_count + 1
+                        continue
 
-        reviews = json_response.get('reviews')
-        if reviews is None:
-            # this is way to high, needs to be looked into and fixed
-            empty_count = empty_count + 1
-            continue
+                    ext_link = review['links'].get('review')
+                    if ext_link is None:
+                        continue
 
-        for review in reviews:
-            review_publication = review['publication']
+                    response = requests.get(ext_link)
+                    soup = BeautifulSoup(response.text)
+                    textList = soup.find_all('p') # find all the paragraphs
 
-            if review_publication not in accepted_publications:
-                forgoed_count = forgoed_count + 1
-                continue
-
-            ext_link = review['links'].get('review')
-            review_ext_links.append(ext_link)
-
-
-    print 'Empty Count ' + str(empty_count)
-    print 'Discarded ' + str(forgoed_count) + ' external reviews'
-    print 'Grabbed ' + str(len(review_ext_links)) + ' external reviews'
-    print 'Writing to XML'
-    for ext_link in review_ext_links:
-        if ext_link is None:
-            continue
-
-        response = requests.get(ext_link)
-        soup = BeautifulSoup(response.text)
-        textList = soup.find_all('p') # find all the paragraphs
-
-        # write out each review out to a file in a directory
-        # this will help us build the proper corpus of rotten tomatoes
-        # documents
-        filename = soup.title.text.replace(' ', '_')
-        filename = re.sub('[^A-Za-z0-9]+', '_', filename)
-        f = open('/Users/abhinavkhanna/Documents/Princeton/Independent Work/rotten_tomato/rotten_tomato_crawler/' + filename + '.xml', 'w')
-        xmlOpen ='<?xml version="1.0" encoding="utf-8"?><?xml-model  href="../schema/digcor.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>'
-        f.write(xmlOpen)
-        f.write('<items>')
-        f.write('<item>')
-        f.write('<date></date><year></year><day></day>')
-        f.write('<solr-datetime></solr-datetime>')
-        f.write('<headline>' + soup.title.text.encode('utf-8') + '</headline>')
-        f.write('<source>Rotten Tomatoes</source>')
-        f.write('<body>')
-        for text in textList:
-            t = re.sub('[^A-Za-z0-9\s]+', '', text.text)
-            f.write(t.encode('utf-8'))
-        f.write('</body>')
-        f.write('</item>')
-        f.write('</items>')
-        f.close()
+                    # write out each review out to a file in a directory
+                    # this will help us build the proper corpus of rotten tomatoes
+                    # documents
+                    filename = soup.title.text.replace(' ', '_')
+                    filename = re.sub('[^A-Za-z0-9]+', '_', filename)
+                    f = open('/Users/abhinavkhanna/Documents/Princeton/Independent Work/rotten_tomato/rotten_tomato_crawler/' + filename + '.xml', 'w')
+                    xmlOpen ='<?xml version="1.0" encoding="utf-8"?><?xml-model  href="../schema/digcor.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>'
+                    f.write(xmlOpen)
+                    f.write('<items>')
+                    f.write('<item>')
+                    f.write('<date></date><year></year><day></day>')
+                    f.write('<solr-datetime></solr-datetime>')
+                    f.write('<headline>' + soup.title.text.encode('utf-8') + '</headline>')
+                    f.write('<source>Rotten Tomatoes</source>')
+                    f.write('<body>')
+                    for text in textList:
+                        t = re.sub('[^A-Za-z0-9\s]+', '', text.text)
+                        f.write(t.encode('utf-8'))
+                    f.write('</body>')
+                    f.write('</item>')
+                    f.write('</items>')
+                    f.close()
 
 
 scrape()
